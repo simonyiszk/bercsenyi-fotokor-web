0b24d39281770b268b24bfcd7bd38a8bd332f7c8
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-18 18:26:58 +0200

    add navbar for desktop
    Signed-off-by: Miklos Daniel <daniel.miklos0@gmail.com>

---
+++ contents/links.ts
@@ -0,0 +1,38 @@
+export const links = {
+  fotokor: {
+    web: "https://fotokor.bercsenyi.bme.hu",
+    fb: "https://www.facebook.com/bercsenyifotokor",
+    ig: "https://www.instagram.com/bmebercsenyifotokor/",
+    email: "bercsenyifotokor@gmail.com",
+  },
+  schdesign: {
+    web: "https://schdesign.hu",
+  },
+};
+
+export const routes = {
+  folytkov: [
+    {
+      name: "folytköv:",
+      href: "/",
+    },
+    {
+      name: "feltöltés",
+      href: "/feltoltes",
+    },
+  ],
+  other: [
+    {
+      name: "rólunk",
+      href: "/rolunk",
+    },
+    {
+      name: "bérlés",
+      href: "/berles",
+    },
+    {
+      name: "profil",
+      href: "/profil",
+    },
+  ],
+};
