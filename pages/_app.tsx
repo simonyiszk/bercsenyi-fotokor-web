4ac2601666c95947184269b0a287fbc7cb9e2e64
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-18 14:23:00 +0200

    replace relative import
    Signed-off-by: Miklos Daniel <daniel.miklos0@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import PageLayout from "@/components/layouts/PageLayout";
 import { AppProps } from "next/app";
 
-import "./../styles/global.css";
+import "@/styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
