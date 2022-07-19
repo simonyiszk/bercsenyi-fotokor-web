2f015348feb02d39da4f059cbac8349cda9095d2
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-19 17:00:31 +0200

    example usage of Listing on RentPage
    Signed-off-by: Miklos Daniel <daniel.miklos0@gmail.com>

--- pages/berles/index.tsx
+++ pages/berles/index.tsx
@@ -1,9 +1,11 @@
 import InnerLayout from "@/components/layouts/InnerLayout";
+import Listing from "@/components/typography/Listing";
 
 export default function RentPage() {
+  const items = ["Zenit 123", "Hasselblad 123", "Canon AE-1"];
   return (
     <InnerLayout title="Bérlés" restrictHeight restrictWidth>
-      <p>Bérlés</p>
+      <Listing title="mit tudsz kölcsönözni tőlünk?" items={items} />
     </InnerLayout>
   );
 }
