7deb0e4e7ad360875af9a18a38cd065ce0fc6c61
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-19 16:59:45 +0200

    add components
    Signed-off-by: Miklos Daniel <daniel.miklos0@gmail.com>

---
+++ components/typography/Listing.tsx
@@ -0,0 +1,21 @@
+import Title from "./Title";
+
+type ListingProps = {
+  title: string;
+  items: string[];
+};
+
+export default function Listing({ title, items }: ListingProps) {
+  return (
+    <div>
+      <Title className="mb-4" type="lowerTitle">
+        {title}
+      </Title>
+      <ul className="list-disc list-inside pl-2 text-xl">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
