add59fb88edd3119ac3273cd963e569dd1ffa66e
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-19 12:35:09 +0200

    init mobile header
    Signed-off-by: Miklos Daniel <daniel.miklos0@gmail.com>

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,13 +5,42 @@ import { routes } from "@/contents/links";
 export default function Header() {
   return (
     <header className="text-2xl">
-      <ul className="flex flex-row gap-4 lg:gap-8 items-center justify-center px-4 lg:px-14 py-8">
+      <div className="hidden md:inline-block w-full">
+        <ul className="flex flex-row gap-4 lg:gap-8 items-center justify-center px-4 lg:px-14 py-8">
+          <Link href={"/"} passHref>
+            <div className="flex flex-row items-center gap-4 cursor-pointer">
+              <Image
+                src={"/logo.png"}
+                width={"40px"}
+                height={"40px"}
+                alt="bercsényi fotókör logo"
+                className="rounded-full"
+                objectFit="cover"
+              />
+              <span>bercsényi fotókör</span>
+            </div>
+          </Link>
+          <hr className="bg-black h-8 lg:h-[40px] w-[2px]" />
+          {routes.folytkov.map(({ name, href }) => (
+            <li key={name}>
+              <Link href={href}>{name}</Link>
+            </li>
+          ))}
+          <hr className="bg-black h-8 lg:h-[40px] w-[2px]" />
+          {routes.other.map(({ name, href }) => (
+            <li key={name}>
+              <Link href={href}>{name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="md:hidden text-2xl px-4 py-4">
         <Link href={"/"} passHref>
           <div className="flex flex-row items-center gap-4 cursor-pointer">
             <Image
               src={"/logo.png"}
-              width={"40px"}
-              height={"40px"}
+              width={"30px"}
+              height={"30px"}
               alt="bercsényi fotókör logo"
               className="rounded-full"
               objectFit="cover"
@@ -19,19 +48,7 @@ export default function Header() {
             <span>bercsényi fotókör</span>
           </div>
         </Link>
-        <hr className="bg-black h-8 lg:h-[40px] w-[2px]" />
-        {routes.folytkov.map(({ name, href }) => (
-          <li key={name}>
-            <Link href={href}>{name}</Link>
-          </li>
-        ))}
-        <hr className="bg-black h-8 lg:h-[40px] w-[2px]" />
-        {routes.other.map(({ name, href }) => (
-          <li key={name}>
-            <Link href={href}>{name}</Link>
-          </li>
-        ))}
-      </ul>
+      </div>
     </header>
   );
 }
