436b3f4e57152021bc57a796bbdc7ebf1c8a77d0
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-19 14:36:23 +0200

    create innerlayout
    Signed-off-by: Miklos Daniel <daniel.miklos0@gmail.com>

---
+++ components/layouts/InnerLayout.tsx
@@ -0,0 +1,42 @@
+import clsx from "clsx";
+import Head from "next/head";
+
+type InnerLayoutProps = {
+  children: JSX.Element | JSX.Element[];
+  restrictWidth?: boolean;
+  restrictHeight?: boolean;
+  title: string;
+  hideTitle?: boolean;
+};
+
+export default function InnerLayout({
+  children,
+  restrictWidth,
+  restrictHeight,
+  title,
+  hideTitle,
+}: InnerLayoutProps) {
+  return (
+    <>
+      <Head>
+        {/* todo seo stuff 
+        https://github.com/simonyiszk/bercsenyi-fotokor-web/milestone/3
+        */}
+        <title>{title.toLocaleLowerCase()} - bercsényi fotókör</title>
+      </Head>
+      <div
+        className={clsx(
+          restrictWidth && "max-w-6xl",
+          restrictHeight && "py-16",
+          "w-full h-full mx-auto"
+        )}
+      >
+        {/* todo replace with Title component - https://github.com/simonyiszk/bercsenyi-fotokor-web/issues/28 */}
+        {!hideTitle && (
+          <h1 className="mb-8 text-4xl font-medium">{title.toLowerCase()}</h1>
+        )}
+        {children}
+      </div>
+    </>
+  );
+}
