f79b979112217f0b21221bc8e307adb8f48015f0
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-19 19:01:14 +0200

    rent page done
    Signed-off-by: Miklos Daniel <daniel.miklos0@gmail.com>

--- pages/berles/index.tsx
+++ pages/berles/index.tsx
@@ -1,11 +1,33 @@
 import InnerLayout from "@/components/layouts/InnerLayout";
 import Listing from "@/components/typography/Listing";
+import Paragraph from "@/components/typography/Paragraph";
+import { links } from "@/contents/links";
+import { loremipsun } from "@/contents/mock";
+import Image from "next/image";
 
 export default function RentPage() {
   const items = ["Zenit 123", "Hasselblad 123", "Canon AE-1"];
   return (
     <InnerLayout title="Bérlés" restrictHeight restrictWidth>
-      <Listing title="mit tudsz kölcsönözni tőlünk?" items={items} />
+      <div className="grid md:grid-cols-2 gap-8 md:gap-16">
+        <div className="flex flex-col gap-8">
+          <Paragraph>{loremipsun.repeat(1)}</Paragraph>
+          <div className="flex flex-col">
+            <span className="text-base">kérdés esetén írj nekünk:</span>
+            <span className="text-xl font-medium">{links.fotokor.email}</span>
+          </div>
+        </div>
+        <div className="w-full h-96 relative aspect-auto">
+          <Image
+            src={"http://placekitten.com/200/300"}
+            layout="fill"
+            alt="darkroom photo"
+            objectFit="cover"
+          />
+        </div>
+
+        <Listing title="mit tudsz kölcsönözni tőlünk?" items={items} />
+      </div>
     </InnerLayout>
   );
 }
