746db8b464d51d6e0ad9c9666728f6662729d9cb
Miklós Dániel <daniel.miklos0@gmail.com>
2022-08-02 00:51:33 +0200

    add model for posts

---
+++ models/post.ts
@@ -0,0 +1,27 @@
+import { DocumentReference } from "firebase/firestore";
+
+export interface IPost {
+  readonly id: string;
+  /**
+   * hopefully cloud functions can handle it
+   * @TODO test this
+   *
+   * range: 1..inf
+   */
+  serial?: number;
+  posted_by: {
+    ref?: DocumentReference;
+    monogramm: string;
+  };
+  readonly url: string;
+  readonly posted_at: Date;
+  /**
+   * image imported from old site
+   */
+  readonly isLegacy?: boolean;
+  isHidden?: boolean;
+  /**
+   * if deleted, the image will be deleted from cloud storage but not from database
+   */
+  isDeleted?: boolean;
+}
