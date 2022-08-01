b7e3a7d3aa095136aae0f9038a4fdb50a64e2e1b
Miklós Dániel <daniel.miklos0@gmail.com>
2022-08-01 23:03:24 +0200

    add custom font

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Main, NextScript, Head } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
