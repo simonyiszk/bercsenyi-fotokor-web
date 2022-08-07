b7ba6fbb69fdc5b2575a80ba513816dd14334742
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2022-08-07 22:57:57 +0200

    fix (#45)

--- components/typography/CustomButton.tsx
+++ components/typography/CustomButton.tsx
@@ -1,37 +1,65 @@
 import clsx from "clsx";
 import React from "react";
-
+import Link from "next/link";
 type CustomButtonType = "black" | "yellow" | "disabled";
 
 export type CustomButtonProps = {
-  variant: CustomButtonType;
-  onClick: () => any;
+  variant?: CustomButtonType;
+  onClick?: () => any;
   children: string;
   className?: string;
   asLink?: boolean;
+  buttonType?: "button" | "a" | "Link";
+  /**
+   * use if its buttonType === "a" or "Link"
+   */
+  href?: string;
 };
 
 const CustomButton = ({
   onClick,
-  variant,
+  variant = "black",
   children,
   className,
+  buttonType = "button",
+  href,
 }: CustomButtonProps) => {
-  return (
-    <button
-      className={clsx(
-        className,
-        variant === "black" && "text-white bg-black",
-        variant === "yellow" && "text-black bg-fotokor-zold",
-        variant === "disabled" && "text-gray-200 bg-black line-through",
-        "rounded-sm px-4 py-1 text-base font-normal"
-      )}
-      onClick={onClick}
-      disabled={variant === "disabled"}
-    >
-      {children}
-    </button>
+  const style = clsx(
+    className,
+    variant === "black" && "text-white bg-black",
+    variant === "yellow" && "text-black bg-fotokor-zold",
+    variant === "disabled" && "text-gray-200 bg-black line-through",
+    "rounded-sm px-4 py-1 text-base font-normal text-center"
   );
+
+  if (["button", "Link"].includes(buttonType)) {
+    if (buttonType === "a") {
+      return (
+        <a href={href} target={"_blank"} rel={"noreferrer"} className={style}>
+          {children}
+        </a>
+      );
+    }
+    if (buttonType === "Link") {
+      return (
+        <Link href={href ?? "/"}>
+          <a className={style}>{children}</a>
+        </Link>
+      );
+    }
+  }
+
+  const cb = React.createElement(
+    buttonType,
+    {
+      className: style,
+      disabled: variant === "disabled",
+      onClick: onClick,
+    },
+    children
+  );
+
+  return cb;
 };
 
 export default CustomButton;
